99c9e7217852c07f8d1328f3c35e6b317fdf3f29
Parenyuk <ParenVic@i.ua>
2024-12-27 19:05:02 +0200

    EDU-004/added Slider for mobile

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -4,7 +4,7 @@ export default async function BookingPage() {
   return (
     <div className='container mx-auto grid grid-cols-1 gap-y-8 md:grid-cols-2 md:gap-20 2xl:gap-24'>
       <AuthSlider />
-      <div className='w-full md:h-[800px] md:w-[540px] 2xl:w-[660px]'>Sign Up</div>
+      <div className='h-[700px] w-full md:h-[800px] md:w-[540px] 2xl:w-[660px]'>Sign Up</div>
     </div>
   );
 }
--- src/components/units/AuthSlider/index.tsx
+++ src/components/units/AuthSlider/index.tsx
@@ -12,9 +12,9 @@ import SwiperSlider from '@/src/components/elements/SwiperSlider';
 
 const AuthSlider = () => {
   return (
-    <div className='flex flex-col justify-center'>
-      <h2 className='md:text-3xl 2xl:text-5xl'>Students Testimonials</h2>
-      <p className='md:mb-15 md:text-base 2xl:mb-20 2xl:text-lg'>
+    <div className='order-last mb-32 flex flex-col justify-center md:order-none'>
+      <h2 className='text-3xl font-medium 2xl:text-5xl'>Students Testimonials</h2>
+      <p className='mb-10 text-sm md:mb-15 md:text-base 2xl:mb-20 2xl:text-lg'>
         Lorem ipsum dolor sit amet consectetur. Tempus tincidunt etiam eget elit id imperdiet et. Cras eu sit dignissim
         lorem nibh et. Ac cum eget habitasse in velit fringilla feugiat senectus in.
       </p>
@@ -28,33 +28,40 @@ const AuthSlider = () => {
           }}
           modules={[Navigation]}
           className='pb-10'
+          spaceBetween={24}
         >
           {sliderMockData.map((item) => (
-            <SwiperSlide key={item.id} className='bg-white-100 md:p-10 2xl:p-12'>
-              <div>{item.description}</div>
-              <div className='mt-4 flex items-center justify-between bg-white-99 md:px-10 md:py-6 2xl:px-8 2xl:py-12'>
+            <SwiperSlide key={item.id} className='border-gray-10 rounded-lg border bg-white-100'>
+              <div className='text-smd p-7 md:p-10 md:text-base 2xl:p-12 2xl:text-lg'>{item.description}</div>
+              <div
+                className={`border-gray-10 mt-4 flex items-center justify-between rounded-bl-lg rounded-br-lg border-t bg-white-99 px-7 py-5
+                md:px-10 md:py-6 2xl:px-8 2xl:py-12`}
+              >
                 <div className='flex items-center'>
                   <Image src={item.authorIcon} alt={item.authorName} width={50} height={50} className='rounded-lg' />
                   <p className='ml-4 font-medium'>{item.authorName}</p>
                 </div>
-                <div className='rounded-lg bg-white-95 px-3.5 py-4'>
-                  <Link href='' color='secondary' className=''>
-                    Read Full Story
+                <div className='rounded-lg border border-white-95 bg-white-97 px-3.5 py-3.5 md:py-4'>
+                  <Link href='' color='secondary' className='text-sm'>
+                    Read More
                   </Link>
                 </div>
               </div>
             </SwiperSlide>
           ))}
         </SwiperSlider>
-        <div className='absolute -bottom-14 right-16 flex justify-end md:space-x-2.5 2xl:space-x-4'>
+        <div
+          className='absolute -bottom-14 left-1/2 flex -translate-x-1/2 justify-center md:left-auto md:right-16 md:translate-x-0
+            md:justify-end md:space-x-2.5 2xl:space-x-4'
+        >
           <button
-            className='swiper-button-prev rounded-lg bg-white-100 !text-black transition hover:bg-gray-300 md:min-h-14 md:min-w-14 md:p-3
+            className='swiper-button-prev min-h-14 min-w-14 rounded-lg bg-white-100 !text-black transition hover:bg-gray-300 md:p-3
               2xl:min-h-16 2xl:min-w-16 2xl:p-3.5'
           >
             →
           </button>
           <button
-            className='swiper-button-next rounded-lg bg-white-100 !text-black transition hover:bg-gray-300 md:min-h-14 md:min-w-14 md:p-3
+            className='swiper-button-next min-h-14 min-w-14 rounded-lg bg-white-100 !text-black transition hover:bg-gray-300 md:p-3
               2xl:min-h-16 2xl:min-w-16 2xl:p-3.5'
           >
             ←
