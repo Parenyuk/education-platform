689041ce25c284d914b1da6ef5c06fdd7c2e7843
Parenyuk <ParenVic@i.ua>
2024-12-27 22:32:19 +0200

    EDU-004/fixed ts error

--- lib/types/components/modules/AuthWrapper/index.tsx
+++ lib/types/components/modules/AuthWrapper/index.tsx
@@ -1,7 +1,5 @@
 import { ReactNode } from 'react';
 
 export type AuthWrapperProps = {
-  title: string;
-  description: string;
   children: ReactNode;
 };
--- src/components/modules/AuthWrapper/index.tsx
+++ src/components/modules/AuthWrapper/index.tsx
@@ -1,13 +1,7 @@
 import { AuthWrapperProps } from '@/lib/types/components/modules/AuthWrapper';
 
-const AuthWrapper = ({ title, description, children }: AuthWrapperProps) => {
-  return (
-    <div className='rounded-xl bg-white-100 p-7 md:p-10 2xl:p-12'>
-      <h2>{title}</h2>
-      <p>{description}</p>
-      {children}
-    </div>
-  );
+const AuthWrapper = ({ children }: AuthWrapperProps) => {
+  return <div className='rounded-xl bg-white-100 p-7 md:p-10 2xl:p-12'>{children}</div>;
 };
 
 export default AuthWrapper;
