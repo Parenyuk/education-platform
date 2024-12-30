3cf89390183aa8f6d3ceef8814801844c9934336
Parenyuk <ParenVic@i.ua>
2024-12-30 13:43:25 +0200

    EDU-005/added forgot password initial page part 2

--- lib/types/schema/SignUpSchema.ts
+++ lib/types/schema/SignUpSchema.ts
@@ -2,4 +2,4 @@ import * as z from 'zod';
 
 import { signUpSchema } from '@/lib/schema/SignUpSchema';
 
-export type SignUpSchema = z.infer<typeof signUpSchema>
+export type SignUpSchema = z.infer<typeof signUpSchema>;
