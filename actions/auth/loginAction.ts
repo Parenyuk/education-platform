000bfda9b8e53647584e57855ac8e3fa022d31a8
Parenyuk <ParenVic@i.ua>
2024-12-30 17:58:29 +0200

    EDU-006/added server validation part 2

--- actions/auth/loginAction.ts
+++ actions/auth/loginAction.ts
@@ -3,16 +3,19 @@
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
+import ZodParseDataAdapter from '@/adapters/zod/ZodParseDataAdapter';
+import { loginSchema } from '@/lib/schema/LoginSchema';
 import { LoginSchema } from '@/lib/types/schema/LoginSchema';
 import { createClient } from '@/utils/supabase/server';
 
 export const loginAction = async (formData: LoginSchema) => {
   console.log('server action', formData);
 
+  const validatedData = ZodParseDataAdapter(loginSchema, formData);
 
   const supabase = await createClient();
 
-  const { data, error } = await supabase.auth.signInWithPassword(formData);
+  const { data, error } = await supabase.auth.signInWithPassword(validatedData.data);
 
   console.log('lo', data);
 
