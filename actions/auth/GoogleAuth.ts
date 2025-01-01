d0d5592725607344a53fae5f0c96d4a7ebbdb941
Parenyuk <ParenVic@i.ua>
2025-01-01 21:58:58 +0200

    EDU-006/fixed bug with no user data

--- actions/auth/GoogleAuth.ts
+++ actions/auth/GoogleAuth.ts
@@ -1,5 +1,6 @@
 'use server';
 
+import { headers } from 'next/headers';
 import { redirect } from 'next/navigation';
 
 import { createClient } from '@/utils/supabase/server';
@@ -7,10 +8,13 @@ import { createClient } from '@/utils/supabase/server';
 export const googleAuth = async () => {
   const supabase = await createClient();
 
+  const origin = await headers().get('origin');
+
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: 'google',
     options: {
-      redirectTo: 'https://fcjzznjdzlmtnndrdsej.supabase.co/auth/v1/callback',
+      // redirectTo: 'https://fcjzznjdzlmtnndrdsej.supabase.co/auth/v1/callback',
+      redirectTo: `${origin}/profile/auth/callback`,
       queryParams: {
         access_type: 'offline',
         prompt: 'consent',
