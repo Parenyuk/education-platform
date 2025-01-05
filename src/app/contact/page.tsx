409ba6add9b3c5569f5737fbd159b0142e6cb159
Parenyuk <ParenVic@i.ua>
2025-01-05 21:21:16 +0200

    EDU-007/fixed layout bug

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -37,8 +37,8 @@ export default function ContactPage() {
   };
 
   return (
-    <>
-      <div className='container mx-auto mt-12 grid md:mt-20 md:grid-cols-2 md:gap-20 2xl:mt-28 2xl:gap-24'>
+    <div className='container mx-auto'>
+      <div className='mt-12 grid md:mt-20 md:grid-cols-2 md:gap-20 2xl:mt-28 2xl:gap-24'>
         <h2 className='text-3xl font-semibold md:self-center md:text-5xl 2xl:text-6xl'>Contact Us</h2>
         <p>{contactPageText}</p>
       </div>
@@ -46,7 +46,7 @@ export default function ContactPage() {
         className='mx-4 mt-7 flex h-0.5 max-w-full items-center justify-center space-x-3 bg-white-90 md:mx-5 md:mt-10 md:space-x-6 2xl:mx-8
           2xl:mt-12'
       />
-      <div className='container mx-auto mb-12 mt-12 flex flex-col rounded-xl bg-white-100 md:mb-20 md:mt-20 md:flex-row 2xl:mb-40 2xl:mt-24'>
+      <div className='mb-12 mt-12 flex flex-col rounded-xl bg-white-100 md:mb-20 md:mt-20 md:flex-row 2xl:mb-40 2xl:mt-24'>
         <form className='w-full p-7 md:p-15 2xl:p-20' onSubmit={handleSubmit(onSubmit)}>
           <div className='grid grid-cols-1 gap-4 pb-4 md:grid-cols-2 md:gap-6 md:pb-6 2xl:gap-8 2xl:pb-8'>
             <FormInput
@@ -92,6 +92,6 @@ export default function ContactPage() {
         </form>
         <ContactBoxUnit />
       </div>
-    </>
+    </div>
   );
 }
