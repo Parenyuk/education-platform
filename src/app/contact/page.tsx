51924636b72d52e476ae758696c689bf212226aa
Parenyuk <ParenVic@i.ua>
2025-01-05 21:01:23 +0200

    EDU-007/added layout to contact form

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Button } from '@nextui-org/button';
@@ -32,8 +32,7 @@ export default function ContactPage() {
   };
 
   const commonInputClassnames = {
-    base: 'border border-gray-50 bg-white-95 rounded-md max-h-15 2xl:h-17',
-    inputWrapper: 'h-15 min-h-15 2xl:h-17 2xl:min-h-17 px-5',
+    inputWrapper: 'border border-gray-15 bg-white-97 h-15 min-h-15 2xl:h-17 2xl:min-h-17 px-5',
     label: 'group-data-[filled-within=true]:-translate-y-[calc(100%_+_theme(fontSize.small)/2_+_30px)]',
   };
 
@@ -47,36 +46,38 @@ export default function ContactPage() {
         className='mx-4 mt-7 flex h-0.5 max-w-full items-center justify-center space-x-3 bg-white-90 md:mx-5 md:mt-10 md:space-x-6 2xl:mx-8
           2xl:mt-12'
       />
-      <div className='container mx-auto mt-12 flex rounded-xl bg-white-100 md:mt-20 2xl:mt-24'>
-        <form className='p-7 md:p-15 2xl:p-20' onSubmit={handleSubmit(onSubmit)}>
-          <FormInput
-            label='Full Name'
-            placeholder='Enter your full name'
-            register={register('fullName', { required: true })}
-            error={errors?.fullName?.message}
-            classNames={commonInputClassnames}
-          />
-          <FormInput
-            label='Email'
-            placeholder='Enter your Email'
-            register={register('email', { required: true })}
-            error={errors?.email?.message}
-            classNames={commonInputClassnames}
-          />
-          <FormInput
-            label='Phone'
-            placeholder='Enter your Phone'
-            register={register('phone', { required: true })}
-            error={errors?.phone?.message}
-            classNames={commonInputClassnames}
-          />
-          <FormInput
-            label='Subject'
-            placeholder='Enter your Subject'
-            register={register('subject', { required: true })}
-            error={errors?.subject?.message}
-            classNames={commonInputClassnames}
-          />
+      <div className='container mx-auto mb-12 mt-12 flex flex-row rounded-xl bg-white-100 md:mb-20 md:mt-20 md:flex-col 2xl:mb-40 2xl:mt-24'>
+        <form className='w-full p-7 md:p-15 2xl:p-20' onSubmit={handleSubmit(onSubmit)}>
+          <div className='grid grid-cols-1 gap-4 pb-4 md:grid-cols-2 md:gap-6 md:pb-6 2xl:gap-8 2xl:pb-8'>
+            <FormInput
+              label='Full Name'
+              placeholder='Enter your full name'
+              register={register('fullName', { required: true })}
+              error={errors?.fullName?.message}
+              classNames={commonInputClassnames}
+            />
+            <FormInput
+              label='Email'
+              placeholder='Enter your Email'
+              register={register('email', { required: true })}
+              error={errors?.email?.message}
+              classNames={commonInputClassnames}
+            />
+            <FormInput
+              label='Phone'
+              placeholder='Enter your Phone'
+              register={register('phone', { required: true })}
+              error={errors?.phone?.message}
+              classNames={commonInputClassnames}
+            />
+            <FormInput
+              label='Subject'
+              placeholder='Enter your Subject'
+              register={register('subject', { required: true })}
+              error={errors?.subject?.message}
+              classNames={commonInputClassnames}
+            />
+          </div>
           <FormInput
             label='Message'
             placeholder='Enter your Message'
