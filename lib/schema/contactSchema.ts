b6ff1d6a5ff18841af2c06002f26c2ecdca08a84
Parenyuk <ParenVic@i.ua>
2025-01-06 23:53:58 +0200

    EDU-007/PhoneInput another library connected

--- lib/schema/contactSchema.ts
+++ lib/schema/contactSchema.ts
@@ -6,7 +6,16 @@ export const contactSchema = z.object({
     .min(8, 'Full name should be at least 8 letters long.')
     .max(50, 'Full name should be max 50 letters long.'),
   email: z.string().email(),
-  phone: z.union([z.string().min(7, 'Phone number must be at least 7 characters long'), z.null()]),
+  phone: z
+    .string()
+    .optional()
+    .refine(
+      (value) => {
+        if (!value || value.length <= 3) return true; // Пропускаємо, якщо тільки код країни
+        return value.replace(/\D+/g, '').length >= 7; // Валідація довжини без нецифрових символів
+      },
+      { message: 'Phone number must be at least 7 digits long.' }
+    ),
   subject: z.string().min(5, 'Subject should be at least 5 letters long.'),
   message: z
     .string()
--- src/components/elements/formElements/FormPhoneInput/index.tsx
+++ src/components/elements/formElements/FormPhoneInput/index.tsx
@@ -1,45 +1,44 @@
 'use client';
 
-import React, { useState } from 'react';
+import React from 'react';
 
-import { parsePhoneNumber } from 'react-phone-number-input';
-import PhoneInputWithCountry from 'react-phone-number-input/react-hook-form';
-import 'react-phone-number-input/style.css';
+import { Controller } from 'react-hook-form';
+import ReactPhoneInput from 'react-phone-input-2';
+import 'react-phone-input-2/lib/style.css';
 
 import { PhoneFormInputProps } from '@/lib/types/components/elements/formElements/FormPhoneInput';
 import FormError from '@/src/components/elements/formElements/FormError';
 
 const FormPhoneInput = ({ control, error }: PhoneFormInputProps) => {
-  const [fullPhoneNumber, setFullPhoneNumber] = useState<string | null>(null);
-
-  const handlePhoneChange = (value?: string) => {
-    if (value) {
-      const phoneNumber = parsePhoneNumber(value);
-
-      if (phoneNumber) {
-        setFullPhoneNumber(phoneNumber.format('E.164'));
-      }
-    } else {
-      setFullPhoneNumber(null);
-    }
-  };
-
   return (
     <div className='relative flex flex-col text-sm font-medium'>
-      <div className='relative -top-0.5 flex items-center'>
-        <label htmlFor='phone'>Phone</label>
-        <p className='ml-1'>{fullPhoneNumber}</p>
-      </div>
-      <PhoneInputWithCountry
-        name='phone'
-        id='phone'
+      <label htmlFor='phone' className='relative -top-0.5'>
+        Phone
+      </label>
+      <Controller
         control={control}
-        autoComplete='new-phone'
-        className='border-gray-15 relative top-0.75 h-15 min-h-15 rounded-xl border bg-white-97 px-5 focus:bg-white-97 2xl:h-17
-          2xl:min-h-17 [&_input]:bg-white-97 [&_input]:outline-none'
-        limitMaxLength
-        onChange={handlePhoneChange}
+        name='phone'
+        render={({ field: { ref, ...field } }) => (
+          <ReactPhoneInput
+            {...field}
+            inputProps={{
+              ref,
+              autoFocus: true,
+              required: false,
+            }}
+            country={'ua'}
+            enableSearch
+            containerClass={`group transition-all !duration-150 flex z-20 border-gray-15 relative top-0.75 h-15 min-h-15 rounded-xl border bg-white-97 
+            focus:bg-white-97 hover:bg-white-100 2xl:h-17 2xl:min-h-17 [&_input]:bg-white-97 [&_input]:outline-none`}
+            buttonClass='!border-none group-hover:bg-white-100 !border-r-[#e5e7eb] !rounded-xl transition-all !duration-150'
+            dropdownClass='!border-none !rounded-xl'
+            inputClass='min-w-full h-full self-center !border-none !group-hover:bg-white-100  !transition-all !duration-150'
+            inputStyle={{ backgroundColor: 'transparent' }}
+            countryCodeEditable={false}
+          />
+        )}
       />
+
       {error && <FormError error={error} />}
     </div>
   );
