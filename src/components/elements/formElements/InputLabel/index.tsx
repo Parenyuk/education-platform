43ee4e7f354be34f4767ef2886eef6dd528ff06a
Parenyuk <ParenVic@i.ua>
2025-01-07 11:52:04 +0200

    EDU-007/returned InputLabel component

---
+++ src/components/elements/formElements/InputLabel/index.tsx
@@ -0,0 +1,8 @@
+import cn from '@/helpers/cn';
+import { InputLabelProps } from '@/lib/types/components/elements/formElements/InputLabel';
+
+const InputLabel = ({ label, error }: InputLabelProps) => {
+  return <label className={cn({ 'text-red-500': error })}>{label}</label>;
+};
+
+export default InputLabel;
