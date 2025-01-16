45c89aa0322e2812195e92ef6043e326897d276f
Parenyuk <ParenVic@i.ua>
2025-01-16 17:16:48 +0200

    EDU-008/user info refactoring for static page, not dynamic

--- src/components/modules/Header/UserInfo/index.tsx
+++ src/components/modules/Header/UserInfo/index.tsx
@@ -1,3 +1,7 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
 import { Link } from '@nextui-org/link';
 
 import { userAuthStateAction } from '@/actions/auth/userAuthStateAction';
@@ -6,18 +10,36 @@ import MenuIcon from '@/public/svg/mobile-menu.svg';
 import LinkAsButton from '@/src/components/elements/LinkAsButton';
 import LogOutButton from '@/src/components/units/LogoutButton';
 
-const UserInfo = async ({}) => {
-  const { isLoggedIn, user } = await userAuthStateAction();
+const UserInfo = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [user, setUser] = useState<{ fullName: string } | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchUserState = async () => {
+      try {
+        const { isLoggedIn, user } = await userAuthStateAction();
+        setIsLoggedIn(isLoggedIn);
+        setUser(user);
+      } catch (error) {
+        console.error('Failed to fetch user state:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchUserState();
+  }, []);
+
+  if (loading) return <div>Loading...</div>;
 
   return (
     <div className='flex items-center justify-center space-x-5'>
       {isLoggedIn ? (
-        <>
-          <div className='mt-2 flex gap-2 rounded-lg bg-white-90 p-3'>
-            <p className='self-center'>{user?.fullName}</p>
-            <LogOutButton />
-          </div>
-        </>
+        <div className='mt-2 flex gap-2 rounded-lg bg-white-90 p-3'>
+          <p className='self-center'>{user?.fullName}</p>
+          <LogOutButton />
+        </div>
       ) : (
         <>
           <Link href={ROUTES.SIGN_UP} color='secondary'>
