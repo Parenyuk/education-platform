14279c89d280bb904890397db603b0838e2c9b10
Parenyuk <ParenVic@i.ua>
2025-02-08 21:41:16 +0200

    EDU-014/deleted unused code

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -1,10 +1,8 @@
-// import { SearchParams } from 'next/dist/server/request/search-params';
 import type { SearchParams } from 'nuqs/server';
 
 import { coursesDescription, coursesTitle } from '@/lib/constants/courses';
 import { MetadataI } from '@/lib/types/common/metadata';
 import { CourseI } from '@/lib/types/components/modules/OurCoursesCardModule';
-import { ExperienceLevelT } from '@/lib/types/components/units/FilterItemsUnit';
 import CoursesList from '@/src/components/modules/CoursesList';
 import FilterIItemsUnit from '@/src/components/units/FilterIItemsUnit';
 import PaginationUnit from '@/src/components/units/PaginationUnit';
@@ -13,20 +11,12 @@ import { fetchData } from '@/supabase/fetchData';
 import { checkLevel } from '@/utils/helpers/checkLevel';
 import { loadSearchParams } from '@/utils/nuqs';
 
-
-export default async function ContactPage({ searchParams }: { searchParams: Promise<SearchParams> }) {
+export default async function CoursesPage({ searchParams }: { searchParams: Promise<SearchParams> }) {
   const { page, level } = await loadSearchParams(searchParams);
-
-  // console.log('page', page);
-
-  // console.log('level', level);
 
   const currentPage = page || 1;
   const itemsPerPage = 5;
   const checkedLevel = checkLevel(level);
-
-
-  console.log('checkedLevel', checkedLevel);
 
   const filters = [];
   if (checkedLevel?.length) {
@@ -41,10 +31,7 @@ export default async function ContactPage({ searchParams }: { searchParams: Prom
   const courses = await fetchData().getAll<CourseI[]>('courses', {
     filters,
     pagination: { limit: itemsPerPage, offset: (currentPage - 1) * itemsPerPage },
-  },
-  );
-  //
-  // console.log('courses', courses);
+  });
 
   const metadataItem = metaData?.data;
 
@@ -59,7 +46,7 @@ export default async function ContactPage({ searchParams }: { searchParams: Prom
   return (
     <>
       <TopPageUnit title={title} description={description} />
-      <FilterIItemsUnit level={(level as ExperienceLevelT[]) || 'all levels'} />
+      <FilterIItemsUnit />
       <CoursesList data={courses.data} />
       <PaginationUnit total={courses.count} />
     </>
