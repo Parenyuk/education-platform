e6a401fb1abc27ee6eedaade4a1e22cfde915ff6
Parenyuk <ParenVic@i.ua>
2025-02-10 18:02:28 +0200

    EDU-015/added initial Course Page

--- src/app/courses/[slug]/page.tsx
+++ src/app/courses/[slug]/page.tsx
@@ -2,11 +2,9 @@ import { CourseI } from '@/lib/types/components/modules/OurCoursesCardModule';
 import SingleCourse from '@/src/components/modules/SingleCourse';
 import { fetchData } from '@/supabase/fetchData';
 
-type CoursePageParams = {
-  slug: string;
-};
+export default async function CoursePage({ params }: { params: Promise<{ slug: string }> }) {
+  const { slug } = await params;
 
-export default async function CoursePage({ params: { slug } }: { params: CoursePageParams }) {
   const { data: course, error } = await fetchData().getOne<CourseI>('courses', 'slug', slug);
 
   if (error || !course) return null;
